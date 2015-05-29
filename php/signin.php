575c4f1f2aee5412de12c8b301ba4ce082f39aff
bossmanbing <bossmanbing@gmail.com>
2015-05-28 22:58:33 -0600

    Collect user_id on sign in.
    Added user_id to sign in query. Catch in $_SESSION variable for ease of
    access later on.

--- php/signin.php
+++ php/signin.php
@@ -4,7 +4,7 @@ require('../include/init.inc.php');
 $username = mysqli_real_escape_string($db, $_POST['username']);
 $password = mysqli_real_escape_string($db, $_POST['password']);
 
-$qry = "SELECT user_name, user_password
+$qry = "SELECT user_id, user_name, user_password
 				FROM user
 					WHERE user_name = '$username'
 					AND user_password = MD5(CONCAT('$password',user_dateJoined))
@@ -14,10 +14,15 @@ $usr_cnt = $qry_res->num_rows;
 
 if ($usr_cnt === 1){
 	// user is logged in.
+	$user = $qry_res->fetch_assoc();
+	$id = $user['user_id'];
+
 	$c_name = 'user';
 	$c_value = $username;
 	setcookie($c_name,$c_value,time()+(86400 * 30), "/"); // 30 day cookie
 	$_SESSION['logged'] = 1;
+	$_SESSION['user'] = $id;
+	
 	header('Location:../add-player.php');
 }
 else{
